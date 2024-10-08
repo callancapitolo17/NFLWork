f39ee73047505c922279d18a7e31f12d1828f038
Cal Cap <capitolo-callan@aramark.com>
2024-10-08 09:31:14 -0700

    scouting reports updates

--- New Scouting Report.R
+++ New Scouting Report.R
@@ -292,8 +292,8 @@ defensive_scouting <- pbp_rp %>%
     away_rush_off_epa = mean(epa[posteam == away_team&pass == 0],na.rm = T)
   )
 
-offense <- "DAL"
-defense <- "PIT"
+offense <- "KC"
+defense <- "NO"
 
 
 team_stats_numeric_off <- offensive_scouting %>% 
