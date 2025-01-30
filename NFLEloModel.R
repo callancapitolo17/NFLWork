ed5d21d3df6f487c1938bb1c1e0066a5b1ac94c7
callancapitolo17 <callancapitolo@yahoo.com>
2025-01-30 15:01:51 -0800

    elo model final

--- NFLEloModel.R
+++ NFLEloModel.R
@@ -1,5 +1,3 @@
-### Bayesian Sports Models in R: Chapter 13 - Bayesian ELO| Andrew Mack | @Gingfacekillah
-
 # Load libraries | Install required packages prior to loading!
 library(ggplot2)        # ggplot: plotting functions
 library(ggridges)       # density ridges plot add-on
@@ -36,9 +34,64 @@ matches <- matches %>%
     home_team_id = team_ids[home_team],
     away_team_id = team_ids[away_team])
 
+data_list <- list(
+  N = nrow(matches),
+  T = length(teams),
+  home_team = matches$home_team_id,
+  away_team = matches$away_team_id,
+  outcome = matches$outcome)
+# Specify Stan model
+stan_model_code <- "
+data {
+  int<lower=0> N;                     // Number of matches
+  int<lower=0> T;                     // Number of teams
+  int<lower=1, upper=T> home_team[N]; // Home team for each match
+  int<lower=1, upper=T> away_team[N]; // Away team for each match
+  int<lower=0, upper=1> outcome[N];   // Outcome of each match
+}
+parameters {
+  vector[T] rating;                   // Ratings for each team
+  real<lower=0> home_adv;             // Home field advantage
+  real<lower=0, upper=100> K;         // ELO sensitivity constant
+}
+model {
+  // Priors
+  rating ~ normal(1500, 50);          // Team rating prior
+  home_adv ~ normal(50, 25);          // Home advantage prior
+  K ~ normal(50, 25);                 // K parameter prior
+
+  for (n in 1:N) {
+    // Likelihood
+    real prob = 1 / (1 + pow(10, (rating[away_team[n]] - (rating[home_team[n]] + home_adv)) / 400));
+    outcome[n] ~ bernoulli(prob);
+  }
+}
+generated quantities {
+  vector[T] new_rating = rating;
+  vector[N] rating_change;
+
+  for (n in 1:N) {
+    // Calculate the probability of home win with ELo formula
+    real prob = 1 / (1 + pow(10, (new_rating[away_team[n]] - (new_rating[home_team[n]] + home_adv)) / 400));
+
+    // Calculate the rating change
+    rating_change[n] = K * (outcome[n] - prob);
+
+    // Apply the rating changes incrementally
+    new_rating[home_team[n]] += rating_change[n];
+    new_rating[away_team[n]] -= rating_change[n];
+  }
+}
+"
+
+# fit Stan model with mcmc
+fit <- stan(model_code = stan_model_code, data = data_list,
+            iter = 2000, warmup = 500, chains = 4, seed = 123)
+
 #### 3. Using win totals to estimate starting ELO ratings ####
 # Gather win totals from reasonably sharp sportsbook and enter them into this vector:, how should I account for different odds?
-win_total_data <- read.csv("Clean Win Totals - Sheet1.csv")
+win_total_data <- read.csv("Clean Win Totals - Sheet1.csv") %>%
+  mutate(NFLfastR.Abbreviation = ifelse(NFLfastR.Abbreviation == "LAR", "LA", NFLfastR.Abbreviation))
 
 # Function to convert American odds to implied probability
 american_odds_to_prob <- function(odds) {
@@ -68,10 +121,6 @@ devig_american_odds <- function(over_odds, under_odds) {
 FD_nv_odds <- devig_american_odds(win_total_data$Fanduel.Over,win_total_data$Fanduel.Under)
 DK_nv_odds <- devig_american_odds(win_total_data$Draftkings.Over,win_total_data$Draftkings.Under)
 
-adjusted_win <- win_total_data %>% 
-  mutate(FD_adjusted_total = (Fanduel.Total+0.5)*(FD_nv_odds$over_prob_no_vig) + (Fanduel.Total-0.5)*(FD_nv_odds$under_prob_no_vig),
-         DK_adjusted_total = (DraftKings.Total+0.5)*(DK_nv_odds$over_prob_no_vig) + (DraftKings.Total-0.5)*(DK_nv_odds$under_prob_no_vig),
-         )
 fd_win_data_list <- list(
   N = nrow(win_total_data),
   total = win_total_data$Fanduel.Total,
@@ -121,24 +170,21 @@ print(fd_win_fit)
 dk_win_fit <- stan(model_code = stan_wins, data = dk_win_data_list,
                    iter = 2000, warmup = 500, chains = 4, seed = 123)
 
-traceplot(dk_win_fit, pars = c("team_wins[1]", "team_wins[2]"))
+traceplot(fd_win_fit, pars = c("team_wins[1]", "team_wins[2]"))
 print(dk_win_fit)
-
-
-# Combine results with the original data
-results <- betting_data %>%
-  mutate(expected_wins = expected_wins)
-
-# Print the results
-print(results)
+fut_win <- data.frame(team = win_total_data$NFLfastR.Abbreviation,
+                      fd_wins = summary(fd_win_fit, pars = c("team_wins"))$summary[,"mean"],
+                      dk_wins = summary(dk_win_fit, pars = c("team_wins"))$summary[,"mean"]) %>%
+  mutate(avg_wins = rowMeans(select(., fd_wins, dk_wins)))
 
 # Normalize win totals
+win_totals <- fut_win$avg_wins
 total_wins <- sum(win_totals)
 normalized_win_totals <- win_totals / total_wins * 272 #<- total wins
 win_probabilities <- normalized_win_totals / 17 #<- total games in Nfl season
 
 # Function to convert win probability to starting ELO rating
-elo_from_win_prob <- function(win_prob, avg_opponent_elo = 1500, home_field_adv = 73) { #Where does 73 come from?
+elo_from_win_prob <- function(win_prob, avg_opponent_elo = 1500, home_field_adv = summary(fit, pars = c("home_adv"))$summary[,"mean"]) {
   team_elo <- avg_opponent_elo + home_field_adv - 400 * log10((1 / win_prob) - 1)
   return(team_elo)
 }
@@ -146,7 +192,7 @@ elo_from_win_prob <- function(win_prob, avg_opponent_elo = 1500, home_field_adv 
 # Calculate starting ELO ratings for each team
 starting_elo_ratings <- sapply(win_probabilities, elo_from_win_prob)
 team_starting_elo <- data.frame(
-  Team = names(starting_elo_ratings),
+  Team = win_total_data$NFLfastR.Abbreviation,
   Starting_Elo_Rating = starting_elo_ratings)
 
 # Print ELO ratings
@@ -156,7 +202,7 @@ print(team_starting_elo)
 starting_elo <- team_starting_elo$Starting_Elo_Rating[match(names(team_ids), team_starting_elo$Team)]
 
 # Prepare data for Stan (using win total priors)
-data_list <- list(
+nfl_data_list <- list(
   N = nrow(matches),
   T = length(teams),
   home_team = matches$home_team_id,
@@ -165,7 +211,7 @@ data_list <- list(
   starting_elo = starting_elo)
 
 # Updated Stan code (using win total priors)
-stan_model_code_new <- "
+stan_model_code_nfl <- "
 data {
     int<lower=0> N;                     // Number of matches
     int<lower=0> T;                     // Number of teams
@@ -210,17 +256,17 @@ generated quantities {
 "
 
 # fit Stan model with mcmc
-fit <- stan(model_code = stan_model_code_new, data = data_list,
+nfl_fit <- stan(model_code = stan_model_code_nfl, data = nfl_data_list,
             iter = 2000, warmup = 500, chains = 4, seed = 123) 
 
 # Print Stan fit
-print(fit) #mean = estimate, se_mean = uncertainty of estimate, sd = variability in posterior, n_eff = number of independent samples, rhat = diagnose convergence, 1 means chains have converged
+print(nfl_fit) #mean = estimate, se_mean = uncertainty of estimate, sd = variability in posterior, n_eff = number of independent samples, rhat = diagnose convergence, 1 means chains have converged
 
 # Print selected parameter trace plots
-traceplot(fit, pars = c("K", "home_adv", "rating[1]", "rating[2]"))
+traceplot(nfl_fit, pars = c("K", "home_adv", "rating[1]", "rating[2]"))
 
 # Print team ELO ratings + home_adv & K parameter estimates
-fit_summary <- summary(fit, pars = c("rating", "home_adv", "K"))$summary
+fit_summary <- summary(nfl_fit, pars = c("rating", "home_adv", "K"))$summary
 rating_estimates <- fit_summary[, "mean"]
 team_ratings <- data.frame(
   Team = names(c(team_ids, "home_adv", "K")),
@@ -231,13 +277,13 @@ print(team_ratings)
 
 #### 4. Plot estimated posterior team strength ####
 # Extract parameters from the fitted model
-fit_summary <- summary(fit, pars = "rating")$summary
+fit_summary <- summary(nfl_fit, pars = "rating")$summary
 rating_estimates <- fit_summary[, "mean"]
 team_ratings <- data.frame(
   Team = names(team_ids),
   ELO_Rating = rating_estimates)
 team_ratings <- team_ratings %>% arrange(desc(ELO_Rating))
-posterior <- rstan::extract(fit)
+posterior <- rstan::extract(nfl_fit)
 rating_samples <- as.data.frame(posterior$rating)
 colnames(rating_samples) <- teams
 rating_samples_long <- tidyr::gather(rating_samples, key = "team", value = "rating")
@@ -266,14 +312,14 @@ team_map
 
 
 #### 5. Simulating future games ####
-predict_match_outcome <- function(home_team_name, away_team_name, n_simulations = 10000) {
+predict_match_outcome <- function(home_team_name, away_team_name, location ="" ,n_simulations = 10000) {
   home_id <- team_ids[home_team_name]
   away_id <- team_ids[away_team_name]
   
   sim_outcomes <- replicate(n_simulations, {
     rating_home <- sample(rating_samples[, home_id], 1)
     rating_away <- sample(rating_samples[, away_id], 1)
-    home_adv <- sample(posterior$home_adv, 1)
+    home_adv <- ifelse(location == "Neutral",0,sample(posterior$home_adv, 1))
     
     win_prob <- 1 / (1 + 10^((rating_away - (rating_home + home_adv)) / 400))
     rbinom(1, 1, win_prob)
@@ -283,10 +329,17 @@ predict_match_outcome <- function(home_team_name, away_team_name, n_simulations 
   return(win_prob_home)
 }
 
-# Example matchup prediction: Detroit at Boston
+test <- matches %>% 
+  # filter(is.na(home_team_score), week == min(week)) %>% 
+  mutate(home_team_win_prob = pmap_dbl(
+    list(home_team, away_team, location), # Ensure 'location' contains "Neutral" or home field info
+    predict_match_outcome),
+         exp_home_odds = 1/home_team_win_prob,
+         exp_home_spread = (log((1/home_team_win_prob)-1)/0.143))
+# Example matchup prediction
 home_team <- "KC"
-away_team <- "BUF"
-win_prob_home <- predict_match_outcome(home_team, away_team)
+away_team <- "PHI"
+win_prob_home <- predict_match_outcome(home_team, away_team, home_adv = "Neutral")
 print(paste("Win probability for", home_team, "against", away_team, ":", win_prob_home))
 
 # Convert win probability to decimal odds
