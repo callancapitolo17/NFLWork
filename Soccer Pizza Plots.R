32bb5063843b84e63bed4087102ae7afbbb84087
Cal Cap <capitolo-callan@aramark.com>
2025-06-19 16:24:31 -0700

    Soccer Plots

---
+++ Soccer Pizza Plots.R
@@ -0,0 +1,41 @@
+library(worldfootballR)
+library(ggshakeR)
+library(dplyr)
+
+
+
+
+pull(player_dictionary_mapping() %>% filter(PlayerFBref  == "Jack Singer") %>% select(UrlFBref))
+data1 <- fb_player_scouting_report(pull(player_dictionary_mapping() %>% filter(PlayerFBref  == "Rafael Leão") %>% select(UrlFBref)), pos_versus = "primary")
+data2 <- fb_player_scouting_report(pull(player_dictionary_mapping() %>% filter(PlayerFBref  == "Nico Williams") %>% select(UrlFBref)), pos_versus = "primary")
+
+strikers <- c(#"Goals","Assists","Non-Penalty Goals",
+              "xG: Expected Goals", "npxG: Non-Penalty xG","Progressive Carries","Progressive Passes",
+              "npxG/Shot","xA: Expected Assists","Shot-Creating Actions","Goal-Creating Actions",
+              "Tackles (Att 3rd)","Interceptions","Touches (Att Pen)","Successful Take-Ons",
+              "Successful Take-On %","Ball Recoveries","Aerials Won","% of Aerials Won")
+
+statgroup_rankings <- data1 %>%
+  count(StatGroup, name = "n") %>%
+  arrange(n) %>%
+  mutate(stat_priority = row_number())
+
+# 3. Join back to the main data
+data <- rbind(data1, data2) %>%
+  filter(Statistic %in% strikers) %>% 
+  filter(scouting_period == "Last 365 Days Men's Big 5 Leagues, UCL, UEL") %>% 
+  left_join(statgroup_rankings, by = "StatGroup") %>%
+  group_by(Player, Statistic) %>%
+  arrange(stat_priority, .by_group = TRUE) %>%
+  slice(1) %>%
+  ungroup() %>% 
+  mutate(Statistic = factor(Statistic, levels = strikers)) %>% 
+  arrange(Player, Statistic)
+  
+
+
+plot_pizza(data = data, type = "comparison", template = "custom",
+                         player_1 = first(data$Player), player_2 = last(data$Player),
+                         season_player_1 = "Last 365 Days Men's Big 5 Leagues, UCL, UEL", 
+                         season_player_2 = "Last 365 Days Men's Big 5 Leagues, UCL, UEL",
+                         color_compare = "red", theme = "black")
