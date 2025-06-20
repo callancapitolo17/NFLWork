a9ae4331cd68f88b1ed1246547d929438aedc594
Cal Cap <capitolo-callan@aramark.com>
2025-06-20 16:55:20 -0700

    function created

--- Soccer Pizza Plots.R
+++ Soccer Pizza Plots.R
@@ -1,31 +1,25 @@
 library(worldfootballR)
 library(ggshakeR)
 library(dplyr)
+library(stringi)
+library(ggplot2)
+library(ggtext)
+player_comps<- function(player1,player2,league1 = "Last 365 Days Men's Big 5 Leagues, UCL, UEL",league2 = "Last 365 Days Men's Big 5 Leagues, UCL, UEL"){
 
-
-
-
-data1 <- fb_player_scouting_report(pull(player_dictionary_mapping() %>% filter(PlayerFBref  == "Rafael Leão") %>% select(UrlFBref)), pos_versus = "primary", time_pause =  10)
-data2 <- fb_player_scouting_report(pull(player_dictionary_mapping() %>% filter(PlayerFBref  == "Nico Williams") %>% select(UrlFBref)), pos_versus = "primary",time_pause =  10)
+data1 <- fb_player_scouting_report(pull(player_dictionary_mapping() %>% filter(PlayerFBref  == player1) %>% select(UrlFBref)%>% slice(1)), pos_versus = "primary", time_pause =  10)
+data2 <- fb_player_scouting_report(pull(player_dictionary_mapping() %>% filter(PlayerFBref  == player2) %>% select(UrlFBref) %>% slice(1)), pos_versus = "primary",time_pause =  10)
 
 strikers <- c(#"Goals","Assists","Non-Penalty Goals",
-              "xG: Expected Goals", "npxG: Non-Penalty xG","Progressive Carries","Progressive Passes",
-              "npxG/Shot","xA: Expected Assists","Shot-Creating Actions","Goal-Creating Actions",
-              "Tackles (Att 3rd)","Interceptions","Touches (Att Pen)","Successful Take-Ons",
-              "Successful Take-On %","Ball Recoveries","Aerials Won","% of Aerials Won")
-
-statgroup_rankings <- rbind(data1, data2) %>%
-  count(StatGroup, name = "n") %>%
-  arrange(n) %>%
-  mutate(stat_priority = row_number())
+              "xG: Expected Goals", "npxG: Non-Penalty xG","npxG/Shot","xA: Expected Assists","Shot-Creating Actions","Goal-Creating Actions",
+              "Progressive Passes", "Progressive Carries","Successful Take-Ons", "Successful Take-On %","Touches (Att Pen)","Aerials Won","% of Aerials Won",
+              "Tackles (Att 3rd)","Interceptions","Ball Recoveries")
 
 # 3. Join back to the main data
 data <- rbind(data1, data2) %>%
+  filter(scouting_period %in% c(league1,league2)) %>% 
   filter(Statistic %in% strikers) %>% 
-  filter(scouting_period == "Last 365 Days Men's Big 5 Leagues, UCL, UEL") %>% 
-  left_join(statgroup_rankings, by = "StatGroup") %>%
   group_by(Player, Statistic) %>%
-  arrange(stat_priority, .by_group = TRUE) %>%
+  arrange(StatGroup, .by_group = TRUE) %>%
   slice(1) %>%
   ungroup() %>% 
   mutate(Statistic = factor(Statistic, levels = strikers)) %>% 
@@ -33,8 +27,42 @@ data <- rbind(data1, data2) %>%
   
 
 
-plot_pizza(data = data, type = "comparison", template = "custom",
-                         player_1 = first(data$Player), player_2 = last(data$Player),
-                         season_player_1 = "Last 365 Days Men's Big 5 Leagues, UCL, UEL", 
-                         season_player_2 = "Last 365 Days Men's Big 5 Leagues, UCL, UEL",
-                         color_compare = "red", theme = "black")
+comp_chart <- plot_pizza(data = data, type = "comparison", template = "custom",
+                         player_1 = stri_trans_general(player1, "Latin-ASCII"), player_2 = stri_trans_general(player2, "Latin-ASCII"),
+                         # season_player_1 = "Last 365 Days Men's Big 5 Leagues, UCL, UEL", 
+                         # season_player_2 = "Last 365 Days Men's Big 5 Leagues, UCL, UEL",
+                         color_compare = "red", theme = "white")+
+  labs(
+    title = paste0(
+      "<span style='color:red'>",player1,"</span> vs ",
+      "<span style='color:black'>",player2,"</span>"
+    ),
+    subtitle = "@CapAnalytics7")+
+  theme(
+    # switch title to element_markdown so those spans get rendered
+    plot.title = element_markdown(size = 20, face = "bold", hjust = 0.5),
+    plot.subtitle = element_markdown(hjust = 0.5, size = 12)
+  )+
+  scale_x_discrete(limits = strikers,labels = function(x) stringr::str_wrap(x, width = 8))
+comp_chart
+ggsave("image.png", width = 4200, height = 2800, units = "px") 
+print(comp_chart)
+}
+player_comps("Milos Kerkez", "Andrew Robertson")
+
+mu25+mu26+plot_layout(ncol = 1)
+
+mu26+   labs(
+  title = paste0(
+    "<span style='color:red'>",player1,"</span> vs ",
+    "<span style='color:black'>",player2,"</span>"
+  ),
+  subtitle = "@CapAnalytics7")+
+  theme(
+    # switch title to element_markdown so those spans get rendered
+    plot.title = element_markdown(size = 20, face = "bold", hjust = 0.5),
+    plot.subtitle = element_markdown(hjust = 0.5, size = 12)
+  )+
+  scale_x_discrete(limits = strikers, labels = function(x) stringr::str_wrap(x, width = 8))
+
+  
