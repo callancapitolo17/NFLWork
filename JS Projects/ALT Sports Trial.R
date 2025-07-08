691c03b20dd555994ece73e643bb8e23b50334f0
Cal Cap <capitolo-callan@aramark.com>
2025-07-08 16:43:54 -0700

    additional work

--- JS Projects/ALT Sports Trial.R
+++ JS Projects/ALT Sports Trial.R
@@ -11,7 +11,8 @@ race_info_unique <- race_info %>%
   distinct(ID, .keep_all = TRUE)
 
 race_sessions_unique <- race_sessions %>% 
-  distinct(ID, .keep_all = TRUE)
+  distinct(ID, .keep_all = TRUE) %>% 
+  rename("Sessions_Name" = Name)
 
 race_competitors_unique <- session_competitors %>% 
   distinct(ID, .keep_all = TRUE)
@@ -19,7 +20,8 @@ race_competitors_unique <- session_competitors %>%
 clean_racing_df <- race_competitors_unique %>% 
   rename("race_competitors_ID" = ID) %>% 
   left_join(race_sessions_unique %>% rename("race_sessions_ID" = ID) , by = c("SessionID" = "race_sessions_ID", "RaceID")) %>% 
-  left_join(race_info_unique %>%  rename("race_info_ID" = ID), by = c("RaceID" = "race_info_ID"))
+  left_join(race_info_unique %>%  rename("race_info_ID" = ID), by = c("RaceID" = "race_info_ID")) %>% 
+  mutate(Position = as.numeric(Position))
 
 #Question 1----
 
@@ -32,12 +34,70 @@ regulars <- clean_racing_df %>%
   mutate(regular = "Yes")
 
 #Question 2----
-clean_racing_df %>% 
+regular_racing_df <- clean_racing_df %>% 
   filter(season == 2024) %>% 
-  left_join(regulars %>% select(fullName,regular), by = c("fullName")) %>% 
-  filter(regular == "Yes") %>% 
-  filter(str_detect(CategoryString, "A Feature"))  
+  left_join(regulars %>% select(fullName,regular), by = c("fullName"))
 
-test <- race_sessions_unique %>% 
-  filter(season == 2024) %>% 
-  
+race_results <- regular_racing_df %>% 
+  filter(regular == "Yes") %>% 
+  filter(str_detect(Sessions_Name,"A Feature")) %>% #check this
+  group_by(fullName) %>% 
+  summarize(wins = mean(Position == 1), top_3 = mean(Position <=3), top_5 = mean(Position <= 5), top_10 = mean(Position <= 10))
+
+
+#Question 3----
+#ChatGPT assistance
+h2h_all <- regular_racing_df %>%
+  filter(regular == "Yes") %>% 
+  filter(str_detect(Sessions_Name,"A Feature")) %>%
+  select(RaceID,SessionID,fullName,Position) %>% 
+  inner_join(regular_racing_df %>%   filter(regular == "Yes") %>% 
+               filter(str_detect(Sessions_Name,"A Feature")) %>% select(RaceID,SessionID,fullName,Position), by = c("RaceID","SessionID"), suffix = (c(".A",".B"))) %>% 
+  filter(fullName.A != fullName.B) %>% 
+  mutate(
+    A_beats_B = case_when(
+      Position.A < Position.B              ~ 1,
+      Position.A > Position.B              ~ 0,
+      Position.A == Position.B             ~ NA_real_,  # tie
+      TRUE                                  ~ NA_real_
+    )
+  )
+
+h2h_matrix <- h2h_all %>%
+  group_by(fullName.A, fullName.B) %>%
+  summarise(
+    races_together = n(),
+    wins           = sum(A_beats_B, na.rm = TRUE),
+    losses         = sum(1 - A_beats_B, na.rm = TRUE),
+    win_pct        = wins / races_together,
+    .groups        = "drop"
+  )
+
+#Question 4----
+non_regular_results <- regular_racing_df %>% 
+  filter(is.na(regular)) %>% 
+  filter(str_detect(Sessions_Name,"A Feature")) %>% #check this
+  group_by(fullName) %>% 
+  summarize(wins = mean(Position == 1), top_3 = mean(Position <=3), top_5 = mean(Position <= 5), top_10 = mean(Position <= 10), 
+            races_count = n_distinct(RaceID)) %>% 
+  filter(fullName != "")
+
+#Question 5
+all_results <- regular_racing_df %>% 
+  filter(str_detect(Sessions_Name,"A Feature")) %>% #check this
+  group_by(fullName) %>% 
+  summarize(wins = mean(Position == 1), top_3 = mean(Position <=3), top_5 = mean(Position <= 5), top_10 = mean(Position <= 10), 
+            races_count = n_distinct(RaceID)) %>% 
+  filter(fullName != "")
+
+start_list_results <- start_list %>% 
+  left_join(all_results, by = "fullName") %>% 
+  mutate(composite_metric = wins*10+top_3*7+top_5*5+top_10)
+#Based on last year's perfromance top contenders are Brad Sweet, Kyle Larson, and Tyler Courtney as the major favorites, then Rico Abreu and Corey Day
+
+#Question 6
+regular25 <- start_list %>% filter(isRegular == 1) %>% 
+  left_join(regulars, by = "fullName") %>% 
+  filter(is.na(regular)) %>% 
+  left_join(all_results, by = "fullName")
+#Aaron Reutzel has experience in Ridge & Sons Racing
