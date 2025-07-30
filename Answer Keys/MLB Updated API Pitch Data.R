b1e3ccc7e77e5cd3621d42c8027f322e714fe4d1
callancapitolo17 <callancapitolo@yahoo.com>
2025-07-30 08:30:23 -0700

    chanes

--- Answer Keys/MLB Updated API Pitch Data.R
+++ Answer Keys/MLB Updated API Pitch Data.R
@@ -79,13 +79,23 @@ pbp_all <- map_dfr(parquet_files, function(file) {
   df
 })
 
-
-write.csv(df_pbp_all,"Pitch_Level_Data_15-24.csv")
-
-df_pbp_all <- read.csv("Pitch_Level_Data_15-24.csv")
-
-games_by_inning <- df_pbp_all %>% 
+#Analysis ----
+game_ml_by_inning <- pbp_all %>% 
+  # filter(!is.na(about.inning)) %>% 
   group_by(game_pk,season,about.inning) %>% 
   summarize(first(game_date),first(home_team), first(away_team),home_score = max(result.homeScore),away_score = max(result.awayScore)) %>% 
-  mutate(home_winner_using_game_score = ifelse(home_score > away_score,1,0)) %>% 
-  pivot_wider(names_from = about.inning, values_from = home_winner_using_game_score)
+  mutate(game_ml_inning_ = ifelse(home_score > away_score,1,ifelse(home_score== away_score,NA,0))) %>% 
+  mutate(full_game_total_inning_ = home_score+away_score) %>% 
+  ungroup() %>% 
+  select(-home_score,-away_score) %>% #allow a proper pivt - score isn't need because of game score
+  pivot_wider(names_from = about.inning, values_from = c(game_ml_inning_,full_game_total_inning_))
+
+
+game_total_by_inning <- pbp_all %>% 
+  filter(!is.na(about.inning)) %>% 
+  group_by(game_pk,season,about.inning) %>% 
+  summarize(first(game_date),first(home_team), first(away_team),home_score = max(result.homeScore),away_score = max(result.awayScore)) %>% 
+  mutate(total = home_score+away_score) %>% 
+  ungroup() %>% 
+  select(-home_score,-away_score) %>% #allow a proper pivt - score isn't need because of game score
+  pivot_wider(names_from = about.inning, values_from = total,names_prefix = "Total")
